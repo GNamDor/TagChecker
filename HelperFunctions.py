edddc9faa46b552485131db2f5adb7dc17615d71
Garab <garab.python@gmail.com>
2025-04-30 23:36:29 +1200

    get_sentences was making new items with ',' but some sentences used ',' in it, and preprocess_sentence was trimed down further

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -6,14 +6,12 @@ def get_sentences(file_path):
 
     f = open(file_path)
     paragraph = f.read()
-    return paragraph.split(",")
+    return paragraph.split('",')
 
 def preprocess_sentence(text):
-    #also removing new lines
-    removed_quotations = text[1:-1].replace('"','')
-    removed_newline = removed_quotations.replace("\n","\n")
+    
     #add padding for > and < 
-    insert_space = removed_newline.replace(">","> ")
+    insert_space = text.replace(">","> ")
     result = insert_space.replace("<", " <")
     return result
     
