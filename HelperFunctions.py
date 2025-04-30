e5f29f5221a04ab79b7e37679cac1d17e3b9685a
Garab <garab.python@gmail.com>
2025-05-01 00:47:42 +1200

    changed closing tag to queue, it looks like last paragaraph returns empty queues, will investigate

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -16,11 +16,11 @@ def preprocess_sentence(text):
     result = insert_space.replace("<", " <")
     return result
     
-def regex_sort(word, op_stack, cl_stack):
+def regex_sort(word, op_stack, cl_queue):
     if re.match("<[A-Z]>", word):
         op_stack.append(word)
     elif re.match("<\/[A-Z]>", word):
-        cl_stack.append(word)
+        cl_queue.append(word)
 
 def remove_slash(word):
     return word[0]+word[2:]    
@@ -28,18 +28,18 @@ def remove_slash(word):
 def add_slash(word):
     return word[0] + "/" + word[1:]   
 
-def output_processing(smallest_stack_size, stack_op_tag, stack_cl_tag):
+def output_processing(smallest_stack_size, stack_op_tag, queue_cl_tag):
     
     for _ in range(smallest_stack_size):
         op = stack_op_tag.pop()
-        cl = stack_cl_tag.pop()
-        print(op," ",cl)
+        cl = queue_cl_tag.popleft()
+        
         if op != remove_slash(cl):
             return f"Expected {add_slash(op)} found {cl}"
             
     if len(stack_op_tag) > 0:
-        return f"Expected {add_slash(stack_cl_tag.pop())} found #"
-    elif len(stack_cl_tag) > 0:
-        return f"Expected # found {stack_cl_tag.pop()}"
+        return f"Expected {add_slash(queue_cl_tag.pop())} found #"
+    elif len(queue_cl_tag) > 0:
+        return f"Expected # found {queue_cl_tag.pop()}"
     else:
         return "Correctly tagged paragraph"
--- TagChecker.py
+++ TagChecker.py
@@ -1,5 +1,5 @@
-from HelperFunctions import get_sentences, preprocess_sentence,regex_sort, remove_slash, add_slash
-
+from HelperFunctions import get_sentences, preprocess_sentence,regex_sort, remove_slash, add_slash, output_processing
+from collections import deque
 
 if __name__ == "__main__":
 
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         #stack 1 for <\w>
         stack_opening_tag = []
         #stack 2 for <\/\w>
-        stack_closing_tag = []
+        queue_closing_tag = deque()
 
         if len(sentence) <1:
             continue
@@ -22,26 +22,10 @@ if __name__ == "__main__":
         for word in words:
             #used https://regex101.com/ for testing
             regex_sort(word, op_stack=stack_opening_tag, 
-                             cl_stack=stack_closing_tag)
+                             cl_queue=queue_closing_tag)
 
-        smallest_stack_size = min(len(stack_opening_tag), len(stack_closing_tag))
+        smallest_stack_size = min(len(stack_opening_tag), len(queue_closing_tag))
 
-        for i in range(smallest_stack_size):
-            op = stack_opening_tag.pop()
-            cl = stack_closing_tag.pop()
-            print(op," ",cl)
-            if op != remove_slash(cl):
-                print(1)
-                print(f"Expected {add_slash(op)} found {cl}")
-                break
-        if len(stack_opening_tag) > 0:
-            print(2)
-            print(f"Expected {add_slash(stack_closing_tag.pop())} found #")
-        elif len(stack_closing_tag) > 0:
-            print(3)
-            print(f"Expected # found {stack_closing_tag.pop()}")
-        else:
-            print(4)
-            print("Correctly tagged paragraph")
-
+        result = output_processing(smallest_stack_size, stack_opening_tag, queue_closing_tag)
+        print(result)
 
