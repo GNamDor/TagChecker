ac650a6e788afbbb71739a79f6f43c9dde01afdc
Garab <garab.python@gmail.com>
2025-05-04 20:41:30 +1200

    more unit test

--- Test.py
+++ Test.py
@@ -13,6 +13,33 @@ class TestMathUtils(unittest.TestCase):
         self.assertEqual(remove_slash("</A>"), "<A>")
         self.assertEqual(remove_slash("</B>"), "<B>")
 
+    def test_add_slash(self):
+        self.assertEqual(add_slash("<A>"), "</A>")
+        self.assertEqual(add_slash("<B>"), "</B>")
+
+    def test_regex_sort_correct(self):
+        words = ['<A>', 'Hello', '</A>']
+        op_stack = []
+        result = regex_sort(words, op_stack)
+        self.assertEqual(result, 'Correctly tagged paragraph",')
+
+    def test_regex_sort_mismatched_tags(self):
+        words = ['<A>', 'Hello', '</B>']
+        op_stack = []
+        result = regex_sort(words, op_stack)
+        self.assertEqual(result, '"Expected </A> found </B>",')
+
+    def test_regex_sort_no_closing_tag(self):
+        words = ['<A>', 'Hello']
+        op_stack = []
+        result = regex_sort(words, op_stack)
+        self.assertEqual(result, '"Expected </A> found #",')
+
+    def test_regex_sort_no_opening_tag(self):
+        words = ['Hello', '</A>']
+        op_stack = []
+        result = regex_sort(words, op_stack)
+        self.assertEqual(result, '"Expected # found </A>",')
 
 
 if __name__ == '__main__':
